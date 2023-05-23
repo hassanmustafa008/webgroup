2314ff98ec195031f331734837cf6897fa3de3cd
Hassan Mustafa <hasaan.mustafa@digitify.com>
2023-05-23 15:15:42 +0500

    testing image in app folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import logo1 from './contact_us.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo1} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>

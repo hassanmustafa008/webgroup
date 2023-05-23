50952b321f33f7ce38697c5a28a7abc917fcc705
Hassan Mustafa <hasaan.mustafa@digitify.com>
2023-05-23 14:57:14 +0500

    testing pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with ease.
+          Learn React
         </a>
       </header>
     </div>
